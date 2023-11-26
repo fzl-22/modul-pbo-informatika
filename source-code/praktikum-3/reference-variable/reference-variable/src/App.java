b98a1c383584bd63dd3f90318c1eaa8341a4d745
Ahmad Faisal <ahmad.faisalewy@gmail.com>
2023-11-27 00:08:01 +0700

    deleted source-code dir and added to .gitignore

--- source-code/praktikum-3/reference-variable/reference-variable/src/App.java
+++
@@ -1,8 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        Manusia manusia = new Manusia();
-         // deklarasi reference variable
-        
-        Manusia mahasiswa = new Mahasiswa();
-    }
-}
